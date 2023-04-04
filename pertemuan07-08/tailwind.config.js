f0047b64a1e8a76445d0bc88318d094f99759b01
Paulus Tindi <arpaulustindi@gmail.com>
2023-04-04 13:03:31 +0800

    Penambahan Assets

---
+++ pertemuan07-08/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      'primary':'#0F172A',
+      'secondary': '#1E293B',
+      'accent': '#7477FF',
+      'text-accent':'#7477FF'
+    }
+  },
+  plugins: [],
+}
+
